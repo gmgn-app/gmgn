9b8a149070189efce0576551a5b3a86f583257c7
ZxStim <thang.victor.luong@gmail.com>
2024-08-30 16:06:13 +0700

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,58 @@
 "use client";
 
 import Image from "next/image";
+import { useState } from "react";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-import { useSendTransaction } from "wagmi";
-import { useSignMessage } from "wagmi";
+import {
+  type BaseError,
+  useSendTransaction,
+  useWaitForTransactionReceipt,
+} from "wagmi";
 import { toHex, fromHex } from "viem";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
-  const { sendTransaction } = useSendTransaction();
-  const { signMessage } = useSignMessage();
-  const message =
-    "went to get a new email, then cross-sold myself into the pass manager, drive, and vpn since they give big discounts for bundling \n OSS + commodity, turned into -> good business with a sticky platform and brand";
+  const {
+    data: hash,
+    error,
+    isPending,
+    sendTransaction,
+  } = useSendTransaction();
+  const [message, setMessage] = useState("");
+
   function submitAndSendMessage() {
     sendTransaction({
       to: "0x1B7a0b3E366CC0549A96ED4123E8058d59282f3f",
       data: toHex(message),
     });
-    console.log(fromHex(toHex(message), "string"));
   }
+
+  function handleInputMessage(e: React.ChangeEvent<HTMLTextAreaElement>) {
+    setMessage(e.target.value);
+  }
+
+  const { isLoading: isConfirming, isSuccess: isConfirmed } = 
+  useWaitForTransactionReceipt({ 
+    hash, 
+  }) 
 
   return (
     <div>
       <ConnectButton />
       <h1>GMGN</h1>
-      <button onClick={submitAndSendMessage}>Send message</button>
+      <Textarea
+        value={message}
+        onChange={handleInputMessage}
+        placeholder="Enter your message"
+      />
+      <Button onClick={submitAndSendMessage}>Send message</Button>
+      {hash && <div>Transaction Hash: {hash}</div>} 
+      {isConfirming && <div>Waiting for confirmation...</div>} 
+      {isConfirmed && <div>Transaction confirmed.</div>} 
+      {error && (
+        <div>Error: {(error as BaseError).shortMessage || error.message}</div>
+      )}
     </div>
   );
 }
