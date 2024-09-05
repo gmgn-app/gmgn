55942aab277e47da05ff964456717d78c0df4873
ZxStim <thang.victor.luong@gmail.com>
2024-09-05 13:33:08 +0700

    fixed home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,24 @@
-"use client";
-
-import { ConnectButton } from '@rainbow-me/rainbowkit';
-import SendMessageDialog from "@/components/send-message-dialog";
+import Link from "next/link";
+import { Signature, MessageSquareText } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-12">
-      <ConnectButton />
-      <SendMessageDialog />
+    <div className="flex flex-col gap-12 justify-center h-screen">
+      <h1 className="text-3xl font-bold">Welcome and have fun!</h1>
+      <Link
+        className="flex flex-row items-center border-2 border-primary rounded-none p-2.5 font-semibold"
+        href="/sign"
+      >
+        <Signature className="mr-2 h-4 w-4" />
+        Sign
+      </Link>
+      <Link
+        className="flex flex-row items-center border-2 border-primary rounded-none p-2.5 font-semibold"
+        href="/message"
+      >
+        <MessageSquareText className="mr-2 h-4 w-4" />
+        Message
+      </Link>
     </div>
   );
 }
---
+++ app/signature/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="flex flex-col gap-12 items-center">
+      Test
+    </div>
+  );
+}
