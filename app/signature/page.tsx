901228b62edab9a1d7ba5c2598707284748e90e7
ZxStim <thang.victor.luong@gmail.com>
2024-09-05 13:47:56 +0700

    added signature

--- app/signature/page.tsx
+++ app/signature/page.tsx
@@ -1,7 +1,21 @@
+'use client'
+ 
+import SignatureDialog from '@/components/signature-dialog'
+import { useSearchParams } from 'next/navigation'
+
 export default function Page() {
+  const searchParams = useSearchParams()
+  const signature = searchParams.get("signature")
+  const account = searchParams.get("account")
+  const timestamp = searchParams.get("timestamp")
+  const signatureProp = {
+    signature: signature,
+    account: account,
+    timestamp: timestamp
+  }
   return (
     <div className="flex flex-col gap-12 items-center">
-      Test
+      <SignatureDialog signatureObject={signatureProp} />
     </div>
   );
 }
---
+++ components/signature-dialog.tsx
@@ -0,0 +1,61 @@
+import { Input } from "./ui/input";
+import { Textarea } from "./ui/textarea";
+
+import { Clock, UserPen, OctagonAlert, Hash } from "lucide-react";
+
+type SignatureObject = {
+  account: string | null;
+  timestamp: string | null;
+  signature: string | null;
+};
+
+export default function SignatureDialog({ signatureObject }: { signatureObject: SignatureObject }) {
+  function unixTimestampToDateTime(unixTimestamp: number) {
+    const date = new Date(unixTimestamp);
+    return date.toLocaleString ? date.toLocaleString() : date.toUTCString();
+  }
+
+  return (
+    <div className="flex flex-col border-black border-2 rounded-md p-4 w-[300px] md:w-[600px] lg:w-[900px]">
+      <h2 className="text-3xl font-semibold mb-4">Signature</h2>
+      <div className="flex flex-col gap-8">
+        <div className="flex flex-col gap-2">
+          <p className="flex flex-row items-center">
+            <UserPen className="mr-2 h-4 w-4" />
+            Signed by
+          </p>
+          <Input
+            className="rounded-none w-full border-black border-2 p-2.5"
+            placeholder="0x..."
+            value={signatureObject.account ? signatureObject.account : ""}
+            readOnly
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <p className="flex flex-row items-center">
+            <Clock className="mr-2 h-4 w-4" />
+            At
+          </p>
+          <Input
+            className="rounded-none w-full border-black border-2 p-2.5"
+            placeholder="0x..."
+            value={unixTimestampToDateTime(signatureObject.timestamp ? parseInt(signatureObject.timestamp) : 0)}
+            readOnly
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <p className="flex flex-row items-center">
+            <Hash className="mr-2 h-4 w-4" />
+            Hash
+          </p>
+          <Textarea
+            className="rounded-none w-full h-36 border-black border-2 p-2.5"
+            placeholder="Enter your message"
+            value={signatureObject.signature ? signatureObject.signature : ""}
+            readOnly
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
