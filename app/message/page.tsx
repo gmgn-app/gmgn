c98ba81108495b6ce5a8d589e80094e951eaabf7
ZxStim <thang.victor.luong@gmail.com>
2024-09-05 13:34:50 +0700

    added nav for message

--- app/message/page.tsx
+++ app/message/page.tsx
@@ -2,11 +2,31 @@
 
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import SendMessageDialog from "@/components/send-message-dialog";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+
 
 export default function Page() {
   return (
     <div className="flex flex-col gap-12 items-center">
       <ConnectButton />
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/">Home</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Message</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
       <SendMessageDialog />
     </div>
   );
