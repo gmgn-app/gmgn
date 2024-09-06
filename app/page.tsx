3a573db8c3f7d6b24ec0ae57db683116edb132b2
ZxStim <thang.victor.luong@gmail.com>
2024-09-06 14:31:52 +0700

    removed wallet link

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,6 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-12 justify-center h-screen">
       <h1 className="text-3xl font-bold">Welcome and have fun!</h1>
-      <a
-        className="flex flex-row items-center border-2 border-primary rounded-none p-2.5 font-semibold"
-        href="https://wallet.gmgn.app"
-      >
-        <Wallet className="mr-2 h-4 w-4" />
-        Wallet
-      </a>
       <Link
         className="flex flex-row items-center border-2 border-primary rounded-none p-2.5 font-semibold"
         href="/sign"
