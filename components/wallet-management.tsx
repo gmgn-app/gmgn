818b11bc88a316a322cb1a159a41c92181818f5b
ZxStim <thang.victor.luong@gmail.com>
2024-09-07 16:11:53 +0700

    fixed buttons

--- components/wallet-management.tsx
+++ components/wallet-management.tsx
@@ -12,12 +12,18 @@ import {
   Account,
   formatEther,
   parseEther,
-  fromBytes
+  fromBytes,
 } from "viem";
 import { klaytnBaobab } from "viem/chains";
 import Image from "next/image";
 import WalletCopyButton from "./wallet-copy-button";
-import { Send, RotateCcw, ArrowDownToLine } from "lucide-react";
+import {
+  Send,
+  RotateCcw,
+  Download,
+  LoaderPinwheel,
+  CirclePlus,
+} from "lucide-react";
 import QRCode from "react-qr-code";
 import {
   Dialog,
@@ -28,13 +34,13 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { useToast } from "@/hooks/use-toast"
-import { ToastAction } from "@/components/ui/toast"
-import { WebAuthnStorage } from "@hazae41/webauthnstorage"
+import { useToast } from "@/hooks/use-toast";
+import { ToastAction } from "@/components/ui/toast";
+import { WebAuthnStorage } from "@hazae41/webauthnstorage";
+import { createIcon } from "@/lib/blockies";
 
 export default function WalletManagement() {
-  const { toast } = useToast()
-
+  const { toast } = useToast();
   const [balance, setBalance] = useState("");
   const [walletAddress, setWalletAddress] = useState("");
   const [walletClient, setWalletClient] = useState<any>();
@@ -43,7 +49,25 @@ export default function WalletManagement() {
 
   const [sendingAmount, setSendingAmount] = useState("");
   const [receivingAddress, setReceivingAddress] = useState("");
+  const [walletName, setWalletName] = useState("");
   const [transactionHash, setTransactionHash] = useState("");
+  const [walletIcon, setWalletIcon] = useState("/kaia.png");
+
+  useEffect(() => {
+    const GMGN_WALLET = localStorage.getItem("gmgn-wallet");
+    if (GMGN_WALLET) {
+      const GMGN_WALLET_STORAGE = JSON.parse(GMGN_WALLET);
+      if (GMGN_WALLET_STORAGE.status === "created") {
+        setCreateWalletButtonActive(false);
+      }
+      if (GMGN_WALLET_STORAGE.icon) {
+        setWalletIcon(GMGN_WALLET_STORAGE.icon);
+      }
+      if (GMGN_WALLET_STORAGE.username) {
+        setWalletName(GMGN_WALLET_STORAGE.username);
+      }
+    }
+  }, []);
 
   const publicClient = createPublicClient({
     chain: klaytnBaobab,
@@ -56,36 +80,6 @@ export default function WalletManagement() {
     });
     setBalance(formatEther(balance).toString());
   }
-
-  // useEffect(() => {
-  //   // const privateKey = localStorage.getItem("privateKey");
-  //   const bytes = await WebAuthnStorage.get(handle)
-  //   const privateKey = fromBytes(
-  //     new Uint8Array(), 
-  //     'hex'
-  //   )
-  //   if (privateKey) {
-  //     setCreateWalletButtonActive(false);
-  //     const account = privateKeyToAccount(privateKey as Address);
-  //     const walletClient = createWalletClient({
-  //       account: privateKeyToAccount(privateKey as Address),
-  //       chain: klaytnBaobab,
-  //       transport: http(),
-  //     });
-  //     setWalletClient(walletClient);
-  //     setWalletAddress(account.address);
-  //     const fetchBalance = async () => {
-  //       const balance = await publicClient.getBalance({
-  //         address: account.address,
-  //       });
-  //       setBalance(formatEther(balance).toString());
-  //     };
-  //     // call the function
-  //     fetchBalance()
-  //       // make sure to catch any error
-  //       .catch(console.error);
-  //   }
-  // }, []);
 
   // Truncate the address for display.
   function truncateAddress(
@@ -110,50 +104,48 @@ export default function WalletManagement() {
     )}...${convertedAddress.slice(-numberOfChars)}`;
   }
 
-    async function getWallet() {
-      
-      /**
-       * Retrieve the handle to the private key from some unauthenticated storage
-       */
-      const cache = await caches.open("my-storage")
-      const request = new Request("my-private-key")
-      const response = await cache.match(request)
-      const handle = response ? new Uint8Array(await response.arrayBuffer()) : new Uint8Array();
-      // const handle = new Uint8Array([156, 237, 69, 251, 193, 186, 47, 79, 7, 235, 149, 213, 83, 235, 149, 107, 155, 176, 52, 240, 51, 62, 173, 205, 28, 234, 252, 16, 219, 138, 124, 143])
-      /**
-       * Retrieve the private key from authenticated storage
-       */
-      const bytes = await WebAuthnStorage.getOrThrow(handle)
-      const privateKey = fromBytes(bytes, 'hex')
-      if (privateKey) {
-        setCreateWalletButtonActive(false);
-        const account = privateKeyToAccount(privateKey as Address);
-        const walletClient = createWalletClient({
-          account: privateKeyToAccount(privateKey as Address),
-          chain: klaytnBaobab,
-          transport: http(),
+  async function getWallet() {
+    /**
+     * Retrieve the handle to the private key from some unauthenticated storage
+     */
+    const cache = await caches.open("my-storage");
+    const request = new Request("my-private-key");
+    const response = await cache.match(request);
+    const handle = response
+      ? new Uint8Array(await response.arrayBuffer())
+      : new Uint8Array();
+    // const handle = new Uint8Array([156, 237, 69, 251, 193, 186, 47, 79, 7, 235, 149, 213, 83, 235, 149, 107, 155, 176, 52, 240, 51, 62, 173, 205, 28, 234, 252, 16, 219, 138, 124, 143])
+    /**
+     * Retrieve the private key from authenticated storage
+     */
+    const bytes = await WebAuthnStorage.getOrThrow(handle);
+    const privateKey = fromBytes(bytes, "hex");
+    if (privateKey) {
+      setCreateWalletButtonActive(false);
+      const account = privateKeyToAccount(privateKey as Address);
+      const walletClient = createWalletClient({
+        account: privateKeyToAccount(privateKey as Address),
+        chain: klaytnBaobab,
+        transport: http(),
+      });
+      setWalletClient(walletClient);
+      setWalletAddress(account.address);
+      const fetchBalance = async () => {
+        const balance = await publicClient.getBalance({
+          address: account.address,
         });
-        setWalletClient(walletClient);
-        setWalletAddress(account.address);
-        const fetchBalance = async () => {
-          const balance = await publicClient.getBalance({
-            address: account.address,
-          });
-          setBalance(formatEther(balance).toString());
-        };
-        // call the function
-        fetchBalance()
-          // make sure to catch any error
-          .catch(console.error);
-      }
+        setBalance(formatEther(balance).toString());
+      };
+      // call the function
+      fetchBalance()
+        // make sure to catch any error
+        .catch(console.error);
     }
-
-
-
+  }
 
   async function createWallet() {
     // const privateKey = generatePrivateKey();
-    const bytes = crypto.getRandomValues(new Uint8Array(32))
+    const bytes = crypto.getRandomValues(new Uint8Array(32));
     /**
      * Store the private key into authenticated storage
      */
@@ -161,24 +153,39 @@ export default function WalletManagement() {
     /**
      * Store the handle to the private key into some unauthenticated storage
      */
-    const cache = await caches.open("my-storage")
-    const request = new Request("my-private-key")
-    const response = new Response(handle)
-    await cache.put(request, response)
+    const cache = await caches.open("my-storage");
+    const request = new Request("my-private-key");
+    const response = new Response(handle);
+    await cache.put(request, response);
+    const icon = createIcon({
+      // All options are optional
+      seed: "randstring", // seed used to generate icon data, default: random
+      color: "#dfe", // to manually specify the icon color, default: random
+      bgcolor: "#aaa", // choose a different background color, default: white
+      size: 15, // width/height of the icon in blocks, default: 10
+      scale: 3, // width/height of each block in pixels, default: 5
+    });
+    const GMGN_WALLET_STORAGE = {
+      status: "created",
+      icon: icon.toDataURL(),
+      username: walletName,
+    };
+    localStorage.setItem("gmgn-wallet", GMGN_WALLET_STORAGE.toString());
+    setCreateWalletButtonActive(false);
   }
 
   async function submitTransaction() {
-    const hash = await walletClient.sendTransaction({ 
+    const hash = await walletClient.sendTransaction({
       to: receivingAddress as Address,
-      value: parseEther(sendingAmount)
-    })
+      value: parseEther(sendingAmount),
+    });
     toast({
       title: "Transaction sent!",
       description: "Hash: " + truncateHash(hash, 6),
       action: <ToastAction altText="view">View</ToastAction>,
-    })
-    setTransactionHash(hash)
-  } 
+    });
+    setTransactionHash(hash);
+  }
 
   return (
     <div className="flex flex-col gap-4">
@@ -186,13 +193,14 @@ export default function WalletManagement() {
         <div className="flex flex-row justify-between">
           <div className="flex flex-col md:flex-row gap-2 items-start">
             <Image
-              src="/avatar.svg"
+              src={walletIcon}
               alt="avatar"
               width={50}
               height={50}
               className="rounded-full border-2 border-primary"
             />
             <div className="flex flex-row gap-2 justify-center items-center">
+              <p>{walletName ? walletName : "---"}</p>
               <p>{truncateAddress(walletAddress as Address, 6)}</p>
               <WalletCopyButton text={walletAddress as Address} />
             </div>
@@ -203,16 +211,40 @@ export default function WalletManagement() {
         </div>
 
         <p className="self-end text-3xl font-semibold">
-          {balance} <span className="text-lg text-gray-400">KLAY</span>
+          {balance ? balance : "-/-"}{" "}
+          <span className="text-lg text-gray-400">KLAY</span>
         </p>
       </div>
-      <div className="flex flex-row gap-2">
+      <div className="grid grid-cols-2 md:flex md:flex-row gap-2">
         {createWalletButtonActive && (
-          <Button className="w-fit" onClick={createWallet}>
-            Create
-          </Button>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button>
+                <CirclePlus className="mr-2 h-4 w-4" />
+                Create
+              </Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader className="flex flex-col items-center">
+                <DialogTitle>Create</DialogTitle>
+                <DialogDescription>Enter note and create</DialogDescription>
+              </DialogHeader>
+              <div>
+                <Input
+                  className="rounded-none w-full border-primary border-2 p-2.5 mt-2"
+                  placeholder="johnsmith"
+                  value={walletName}
+                  onChange={(e) => setWalletName(e.target.value)}
+                />
+              </div>
+              <DialogFooter>
+                <Button onClick={createWallet}>Create</Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
         )}
-        <Button className="w-fit" onClick={getWallet}>
+        <Button onClick={getWallet}>
+          <LoaderPinwheel className="mr-2 h-4 w-4" />
           Load
         </Button>
         <Dialog>
@@ -225,12 +257,10 @@ export default function WalletManagement() {
           <DialogContent>
             <DialogHeader className="flex flex-col items-center">
               <DialogTitle>Send</DialogTitle>
-              <DialogDescription>
-                Enter address and amount
-              </DialogDescription>
+              <DialogDescription>Enter address and amount</DialogDescription>
             </DialogHeader>
             <div>
-              <Input 
+              <Input
                 className="rounded-none w-full border-primary border-2 p-2.5 mt-2"
                 placeholder="0x..."
                 value={receivingAddress}
@@ -251,7 +281,7 @@ export default function WalletManagement() {
         <Dialog>
           <DialogTrigger asChild>
             <Button>
-              <ArrowDownToLine className="mr-2 h-4 w-4" />
+              <Download className="mr-2 h-4 w-4" />
               Receive
             </Button>
           </DialogTrigger>
