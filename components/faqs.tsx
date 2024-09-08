53b4dc5a2a2fa113705d05a3dca7d1dfd3a29908
ZxStim <thang.victor.luong@gmail.com>
2024-09-08 16:29:04 +0700

    update shadcn

--- components/faqs.tsx
+++ components/faqs.tsx
@@ -13,13 +13,19 @@ export default function Faqs() {
         <AccordionItem value="item-1">
           <AccordionTrigger>Is GMGN wallet safe?</AccordionTrigger>
           <AccordionContent>
-            Yes. GMGN wallet is a non-custodial wallet, which means you are in control. Your private keys are stored locally in your device Secured Enclave and never leave it.
+            Yes. GMGN wallet is a non-custodial wallet, which means you are in control. Your private key is stored locally in your device Secured Enclave and never leaves it.
           </AccordionContent>
         </AccordionItem>
         <AccordionItem value="item-2">
-          <AccordionTrigger>I just created my wallet, what's next?</AccordionTrigger>
+          <AccordionTrigger>I just created my wallet, what&apos;s next?</AccordionTrigger>
           <AccordionContent>
-            At the moment, after creating your wallet, you have to click "Load" button to load your wallet. Everytime you access the website after wallet creation, you also have to load your wallet. We are working on improving this process.
+            At the moment, after creating your wallet, you have to click &quot;Load&quot; button to load your wallet. Everytime you access the website after wallet creation, you also have to load your wallet. We are working on improving this process.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-3">
+          <AccordionTrigger>Things are not loading automatically</AccordionTrigger>
+          <AccordionContent>
+            At the moment, every interactions require you to click a button to initiate. We are working on improving this process.
           </AccordionContent>
         </AccordionItem>
       </Accordion>
