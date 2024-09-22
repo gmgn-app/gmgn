a219aed219ee74c216c826c987558322b37a4f3a
ZxStim <thang.victor.luong@gmail.com>
2024-09-22 22:42:56 +0700

    added delegate fee

--- app/send/page.tsx
+++ app/send/page.tsx
@@ -1,12 +1,23 @@
 import SendTransactionForm from "@/components/send-transaction-form";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function SendPage() {
   return (
     <div className="flex flex-col gap-12 p-4 w-screen md:w-[768px]">
+      <Link href="/">
+        <Image
+          src="/gmgn-logo.svg"
+          alt="gmgn logo"
+          width={40}
+          height={40}
+          className="rounded-md"
+        />
+      </Link>
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
         Send
       </h1>
       <SendTransactionForm />
     </div>
-  )
+  );
 }
--- components/send-transaction-form.tsx
+++ components/send-transaction-form.tsx
@@ -1,12 +1,11 @@
 "use client";
 
-import { useState } from "react";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
+import { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Switch } from "@/components/ui/switch";
 import {
   Form,
   FormControl,
@@ -16,80 +15,339 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-
-const formSchema = z.object({
-  receiver: z.string().min(2).max(50),
-  amount: z.number().min(0),
-  memo: z.string().max(500),
-});
+import { useSearchParams } from "next/navigation";
+import {
+  createPublicClient,
+  http,
+  Address,
+  formatEther,
+  fromBytes,
+  createWalletClient,
+  parseEther,
+  toHex,
+} from "viem";
+import { Wallet, JsonRpcProvider, TxType } from "@kaiachain/ethers-ext";
+import { klaytnBaobab } from "viem/chains";
+import { privateKeyToAccount } from "viem/accounts";
+import { getOrThrow } from "@/lib/passkey-auth";
+import { useToast } from "@/hooks/use-toast";
+import { ToastAction } from "@/components/ui/toast";
+import { Send, RotateCcw } from "lucide-react";
+import { setBalance } from "viem/actions";
 
 export default function SendTransactionForm() {
-  // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      receiver: "",
-      amount: 0,
-      memo: "",
-    },
-  });
+  // Get the search params from the URL.
+  const searchParams = useSearchParams();
+  const chainName = searchParams.get("chain");
+  const address = searchParams.get("address");
+  const balance = searchParams.get("balance");
+  const [currentBalance, setCurrentBalance] = useState(formatEther(BigInt(balance!)));
+  const [sendingAmount, setSendingAmount] = useState("");
+  const [receivingAddress, setReceivingAddress] = useState("");
+  const [sendingMessage, setSendingMessage] = useState("");
+  const [gasEstimate, setGasEstimate] = useState("");
+  const [gasPrice, setGasPrice] = useState("");
+  const [transactionCost, setTransactionCost] = useState("");
+  const [readyToTransfer, setReadyToTransfer] = useState(false);
+  const [delegateFeeActive, setDelegateFeeActive] = useState(false);
+
+  // Toast notifications.
+  const { toast } = useToast();
+
+  // Truncate the hash for display
+  function truncateHash(address: String | undefined, numberOfChars: number) {
+    if (!address) return "No address";
+    let convertedAddress = address.toString();
+    return `${convertedAddress.slice(
+      0,
+      numberOfChars
+    )}...${convertedAddress.slice(-numberOfChars)}`;
+  }
+
+  // Select the block explorer based on the network
+  function selectBlockExplorer(network: string | undefined) {
+    switch (network) {
+      case "kaia":
+        return "https://kaiascan.io";
+      case "kaia-kairos":
+        return "https://kairos.kaiascan.io";
+      default:
+        return "https://sepolia.etherscan.io";
+    }
+  }
+
+  function formatBalance(number: string, maxDecimal: number) {
+    // split the number base on the decimal point, then take only maxDecimals character from the decimal part
+    const [whole, decimal] = number.split(".");
+    const formattedWhole = whole.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return `${formattedWhole}.${decimal.slice(0, maxDecimal)}`;
+  }
+
+  function selectJsonRpcProvider(network: string | undefined) {
+    // https://rpc.ankr.com/arbitrum_sepolia
+    // https://rpc.ankr.com/base_sepolia
+    switch (network) {
+      case "kaia":
+        return new JsonRpcProvider("https://public-en-kaia.node.kaia.io");
+      case "kaia-kairos":
+        return new JsonRpcProvider("https://public-en-kairos.node.kaia.io");
+      default:
+        return new JsonRpcProvider("https://public-en-kairos.node.kaia.io");
+    }
+  }
+
+  function selectNativeAssetSymbol(network: string | undefined | null) {
+    switch (network) {
+      case "kaia":
+        return "KLAY";
+      case "kaia-kairos":
+        return "KLAY";
+      case "arbitrum-sepolia":
+        return "ETH";
+      case "base-sepolia":
+        return "ETH";
+      case "ethereum-sepolia":
+        return "ETH";
+      default:
+        return "ETH";
+    }
+  }
+
+  async function fetchBalance() {
+    const publicClient = createPublicClient({
+      chain: klaytnBaobab,
+      transport: http(),
+    });
+    const balance = await publicClient.getBalance({
+      address: address as Address,
+    });
+    setCurrentBalance(formatEther(balance).toString());
+  }
+
+
+  async function fetchTransactionCostEstimate() {
+    const publicClient = createPublicClient({
+      chain: klaytnBaobab,
+      transport: http(),
+    });
+    const gas = await publicClient.estimateGas({
+      account: address as Address,
+      to: receivingAddress as Address,
+      value: parseEther(sendingAmount),
+    });
+    const gasPrice = await publicClient.getGasPrice();
+    setGasEstimate(formatEther(gas));
+    setGasPrice(formatEther(gasPrice));
+    setTransactionCost(formatEther(gas * gasPrice));
+    setReadyToTransfer(true);
+  }
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+  async function submitTransaction() {
+    /**
+     * Retrieve the handle to the private key from some unauthenticated storage
+     */
+    const cache = await caches.open("gmgn-storage");
+    const request = new Request("gmgn-wallet");
+    const response = await cache.match(request);
+    const handle = response
+      ? new Uint8Array(await response.arrayBuffer())
+      : new Uint8Array();
+    /**
+     * Retrieve the private key from authenticated storage
+     */
+    const bytes = await getOrThrow(handle);
+    const privateKey = fromBytes(bytes, "hex");
+    if (privateKey) {
+      const account = privateKeyToAccount(privateKey as Address);
+      const walletClient = createWalletClient({
+        account: privateKeyToAccount(privateKey as Address),
+        chain: klaytnBaobab,
+        transport: http(),
+      });
+      const hash = await walletClient.sendTransaction({
+        account,
+        to: receivingAddress as Address,
+        value: BigInt(sendingAmount),
+      });
+      toast({
+        className:
+          "bottom-0 right-0 flex fixed md:max-h-[300px] md:max-w-[420px] md:bottom-4 md:right-4",
+        title: "Transaction sent!",
+        description: "Hash: " + truncateHash(hash, 6),
+        action: (
+          <ToastAction altText="view">
+            <a
+              target="_blank"
+              href={`${selectBlockExplorer(chainName!)}/tx/${hash}`}
+            >
+              View
+            </a>
+          </ToastAction>
+        ),
+      });
+    } else {
+      toast({
+        className:
+          "bottom-0 right-0 flex fixed md:max-h-[300px] md:max-w-[420px] md:bottom-4 md:right-4",
+        variant: "destructive",
+        title: "Wallet load failed!",
+        description: "Uh oh! Something went wrong. please try again.",
+      });
+    }
+  }
+  // =========== END OF FORM HANDLING ===========
+
+  function handleDelegateFeeChange() {
+    setDelegateFeeActive(!delegateFeeActive);
+    setReadyToTransfer(!readyToTransfer);
+  }
+
+  async function submitDelegatedTransaction() {
+    /**
+     * Retrieve the handle to the private key from some unauthenticated storage
+     */
+    const cache = await caches.open("gmgn-storage");
+    const request = new Request("gmgn-wallet");
+    const response = await cache.match(request);
+    const handle = response
+      ? new Uint8Array(await response.arrayBuffer())
+      : new Uint8Array();
+    /**
+     * Retrieve the private key from authenticated storage
+     */
+    const bytes = await getOrThrow(handle);
+    const privateKey = fromBytes(bytes, "hex");
+    const provider = selectJsonRpcProvider(chainName!);
+    const kaiaSdkWalletClient = new Wallet(privateKey, provider);
+    let tx = {
+      type: TxType.FeeDelegatedValueTransferMemo,
+      to: receivingAddress,
+      from: address as Address,
+      value: parseEther(sendingAmount),
+      input: toHex(sendingMessage),
+    };
+    const preparedTx = await kaiaSdkWalletClient.populateTransaction(tx);
+    const hash = await kaiaSdkWalletClient.signTransaction(preparedTx);
+    const currentNetwork = chainName;
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/delegate-fee`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            signature: hash,
+            network: currentNetwork,
+          }),
+        }
+      );
+      const result = await response.json();
+      toast({
+        className:
+          "bottom-0 right-0 flex fixed md:max-h-[300px] md:max-w-[420px] md:bottom-4 md:right-4",
+        title: "Transaction sent!",
+        description: "Hash: " + truncateHash(result.receipt.transactionHash, 6),
+        action: (
+          <ToastAction altText="view">
+            <a
+              target="_blank"
+              href={`${selectBlockExplorer(chainName!)}/tx/${
+                result.receipt.transactionHash
+              }`}
+            >
+              View
+            </a>
+          </ToastAction>
+        ),
+      });
+    } catch (error) {
+      toast({
+        className:
+          "bottom-0 right-0 flex fixed md:max-h-[300px] md:max-w-[420px] md:bottom-4 md:right-4",
+        variant: "destructive",
+        title: "Transaction failed!",
+        description: "Uh oh! Something went wrong.",
+        action: <ToastAction altText="Try again">Try again</ToastAction>,
+      });
+    }
+    setReadyToTransfer(true);
+    setReceivingAddress("");
+    setSendingMessage("");
   }
 
   return (
     <div className="flex flex-col">
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <FormField
-            control={form.control}
-            name="receiver"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Receiver</FormLabel>
-                <FormControl>
-                  <Input placeholder="0xADeC..." {...field} />
-                </FormControl>
-                <FormDescription>Address of the recipient.</FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
+      <div>
+        <h2 className="text-xl">Balance</h2>
+        <div className="flex flex-row items-center justify-between">
+          <p className="text-2xl">
+            {currentBalance ? formatBalance(currentBalance, 4) : "-/-"}{" "}
+            <span className="text-lg">
+              {selectNativeAssetSymbol(chainName)}
+            </span>
+          </p>
+          <Button onClick={fetchBalance} size="icon">
+            <RotateCcw className="w-4 h-4" />
+          </Button>
+        </div>
+      </div>
+      <div className="flex flex-col gap-8 mt-4 mb-6">
+        <div className="flex flex-col gap-2">
+          <Label htmlFor="receivingAddress">Receiving address</Label>
+          <Input
+            id="receivingAddress"
+            className="rounded-none w-full border-primary border-2 p-2.5 mt-2"
+            placeholder="0x..."
+            value={receivingAddress}
+            onChange={(e) => setReceivingAddress(e.target.value)}
           />
-          <FormField
-            control={form.control}
-            name="amount"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Amount</FormLabel>
-                <FormControl>
-                  <Input placeholder="1" {...field} />
-                </FormControl>
-                <FormDescription>Address of the recipient.</FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
+        </div>
+        <div className="flex flex-col gap-2">
+          <Label htmlFor="sendingAmount">Amount</Label>
+          <Input
+            id="sendingAmount"
+            className="rounded-none w-full border-primary border-2 p-2.5 mt-2"
+            placeholder="0"
+            value={sendingAmount}
+            onChange={(e) => setSendingAmount(e.target.value)}
           />
-          <FormField
-            control={form.control}
-            name="memo"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Memo</FormLabel>
-                <FormControl>
-                  <Input placeholder="gm & gn" {...field} />
-                </FormControl>
-                <FormDescription>Message to the recipient.</FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
+        </div>
+        <div className="flex items-center space-x-2">
+          <Switch
+            id="delegate-fee"
+            checked={delegateFeeActive}
+            onCheckedChange={handleDelegateFeeChange}
           />
-          <Button type="submit">Send transaction</Button>
-        </form>
-      </Form>
+          <Label htmlFor="delegate-fee">Delegate fee</Label>
+        </div>
+
+        {!delegateFeeActive && (
+          <div className="flex flex-col gap-2 border-2 border-primary p-2 text-right">
+            <h2 className="border-b pb-2 text-xl font-semibold">Details</h2>
+            <p>{gasEstimate} : Gas</p>
+            <p>{gasPrice} : Gas price</p>
+            <p>{transactionCost} : Cost</p>
+            <Button
+              disabled={readyToTransfer}
+              className="w-fit self-end"
+              onClick={fetchTransactionCostEstimate}
+            >
+              Continue
+            </Button>
+          </div>
+        )}
+      </div>
+      <Button
+        disabled={!readyToTransfer}
+        onClick={
+          delegateFeeActive ? submitDelegatedTransaction : submitTransaction
+        }
+      >
+        Send
+      </Button>
     </div>
   );
 }
