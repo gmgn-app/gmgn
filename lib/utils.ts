7399f35d1add5cee3ab13b6583ced16787190bc9
ZxStim <thang.victor.luong@gmail.com>
2024-09-23 18:16:45 +0700

    edit utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,23 +1,36 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
-import { Address } from "viem"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { Address } from "viem";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
-  // Truncate the address for display.
+// Truncate the address for display.
 export function truncateAddress(
-    address: Address | undefined,
-    numberOfChars: number
-  ) {
-    if (!address) return "--------------";
-    let convertedAddress = address.toString();
-    return `${convertedAddress.slice(
-      0,
-      numberOfChars
-    )}...${convertedAddress.slice(-4)}`;
-  }
+  address: Address | undefined,
+  numberOfChars: number
+) {
+  if (!address) return "--------------";
+  let convertedAddress = address.toString();
+  return `${convertedAddress.slice(
+    0,
+    numberOfChars
+  )}...${convertedAddress.slice(-4)}`;
+}
+
+// Truncate the hash for display
+export function truncateHash(
+  address: String | undefined,
+  numberOfChars: number
+) {
+  if (!address) return "--------------";
+  let convertedAddress = address.toString();
+  return `${convertedAddress.slice(
+    0,
+    numberOfChars
+  )}...${convertedAddress.slice(-numberOfChars)}`;
+}
 
 export function selectChainNameFromNetwork(network: string | null) {
   if (!network) return "Unknown Network";
