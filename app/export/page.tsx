dc59d52f2e06028fe294a86c18f74e1c0538b2b4
ZxStim <thang.victor.luong@gmail.com>
2024-09-26 16:02:21 +0700

    added export page

--- app/export/page.tsx
+++ app/export/page.tsx
@@ -4,27 +4,12 @@ import Link from "next/link";
 import Image from "next/image";
 import BackButton from "@/components/back-button";
 import { Button } from "@/components/ui/button";
-import { OctagonX, CircleArrowLeft } from "lucide-react";
+import { ScanEye, CircleArrowLeft } from "lucide-react";
 import { useToast } from "@/hooks/use-toast";
 import { ToastAction } from "@/components/ui/toast";
 
 export default function DeletePage() {
   const { toast } = useToast();
-
-  function deleteLocalWallet() {
-    localStorage.removeItem("gmgn-wallet");
-    toast({
-      className:
-        "bottom-0 right-0 flex fixed md:max-h-[300px] md:max-w-[420px] md:bottom-4 md:right-4",
-      title: "Wallet deleted!",
-      description: "Go back to home to create new wallet",
-      action: (
-        <ToastAction altText="Home">
-          <Link href="/">Home</Link>
-        </ToastAction>
-      ),
-    });
-  }
 
   return (
     <div className="flex flex-col gap-6 p-4 w-screen md:w-[768px]">
@@ -38,17 +23,16 @@ export default function DeletePage() {
         />
       </Link>
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-        Delete wallet
+        Export wallet
       </h1>
       <BackButton route={null} />
       <h2 className="text-2xl font-semibold mt-6">
-        This action is non-reversible, are you sure that you want to delete this
-        wallet?
+        When you export wallet, you will see your private key. Make sure to only do this in a private place.
       </h2>
       <div className="flex flex-col gap-24">
-        <Button onClick={deleteLocalWallet} variant="destructive">
-          <OctagonX className="w-4 h-4 mr-2" />
-          Yes! Delete the wallet now
+        <Button variant="destructive">
+          <ScanEye className="w-4 h-4 mr-2" />
+          Yes! Show me my private key
         </Button>
         <Button asChild>
           <Link href="/">
