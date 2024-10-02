4f76f00dde318a4eec36fee91148832f9b339df2
ZxStim <thang.victor.luong@gmail.com>
2024-10-02 13:43:52 +0700

    improved pay form

--- components/pay-form.tsx
+++ components/pay-form.tsx
@@ -5,7 +5,6 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Label } from "@/components/ui/label";
-import { Skeleton } from "@/components/ui/skeleton";
 import { Switch } from "@/components/ui/switch";
 import { useSearchParams } from "next/navigation";
 import {
@@ -18,7 +17,6 @@ import {
   parseEther,
   toHex,
   fromHex,
-  getAddress,
   isAddress,
 } from "viem";
 import { Wallet, TxType } from "@kaiachain/ethers-ext";
@@ -27,26 +25,13 @@ import { getOrThrow } from "@/lib/passkey-auth";
 import { useToast } from "@/hooks/use-toast";
 import { ToastAction } from "@/components/ui/toast";
 import {
-  Send,
   RotateCcw,
   Fuel,
   Loader2,
-  ScanLine,
-  ThumbsUp,
   Check,
   CircleX,
-  Ban
+  HandCoins,
 } from "lucide-react";
-import { Scanner } from "@yudiel/react-qr-scanner";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogFooter,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
 import {
   Popover,
   PopoverContent,
@@ -61,8 +46,8 @@ import {
   selectViemChainFromNetwork,
   selectNativeAssetSymbol,
   selectJsonRpcProvider,
+  selectChainNameFromNetwork,
 } from "@/lib/utils";
-
 
 export default function PayForm() {
   // Get the search params from the URL.
@@ -74,16 +59,18 @@ export default function PayForm() {
     redirect("/");
   }
 
+  const sendingAmount = searchParams.get("sendingAmount") || "";
+  const receivingAddress = searchParams.get("receivingAddress") || "";
+  const transactionMemo =
+    fromHex(searchParams.get("transactionMemo") as `0x${string}`, "string") ||
+    "";
+
   const [currentBalance, setCurrentBalance] = useState("");
-  const [sendingAmount, setSendingAmount] = useState(searchParams.get("sendingAmount") || "");
-  const [receivingAddress, setReceivingAddress] = useState(searchParams.get("receivingAddress") || "");
-  const [transactionMemo, setTransactionMemo] = useState(fromHex(searchParams.get("transactionMemo") as `0x${string}`, 'string') || "");
   const [transactionCost, setTransactionCost] = useState("");
   const [readyToTransfer, setReadyToTransfer] = useState(false);
   const [delegateFeeActive, setDelegateFeeActive] = useState(false);
   const [continueButtonLoading, setContinueButtonLoading] = useState(false);
   const [sendButtonLoading, setSendButtonLoading] = useState(false);
-  const [qrScanSuccess, setQrScanSuccess] = useState(false);
   const [isValidAddress, setIsValidAddress] = useState<Boolean | undefined>(
     undefined
   );
@@ -128,25 +115,6 @@ export default function PayForm() {
     setCurrentBalance(formatEther(balance).toString());
   }
 
-  function handleQrScan(data: string) {
-    if (data.includes(":")) {
-      const splitData = data.split(":");
-      setReceivingAddress(splitData[1]);
-      setQrScanSuccess(true);
-      // delay the success message for 2 seconds
-      setTimeout(() => {
-        setQrScanSuccess(false);
-      }, 5000);
-    } else {
-      setReceivingAddress(data);
-      setQrScanSuccess(true);
-      // delay the success message for 2 seconds
-      setTimeout(() => {
-        setQrScanSuccess(false);
-      }, 5000);
-    }
-  }
-
   function handleDelegateFeeChange() {
     setDelegateFeeActive(!delegateFeeActive);
     setReadyToTransfer(true);
@@ -184,9 +152,9 @@ export default function PayForm() {
     if (sendingAmount) {
       const isValidAmount =
         parseEther(currentBalance) >= parseEther(sendingAmount);
-        if (isValidAmount) {
-          setIsValidAmount(true);
-          setContinueButtonLoading(true);
+      if (isValidAmount) {
+        setIsValidAmount(true);
+        setContinueButtonLoading(true);
         const publicClient = createPublicClient({
           chain: selectViemChainFromNetwork(network!),
           transport: http(),
@@ -285,6 +253,61 @@ export default function PayForm() {
     setReadyToTransfer(false);
   }
 
+  // Function to prepare transaction before sending
+  async function prepareDelegatedTransaction() {
+    if (receivingAddress === "") {
+      toast({
+        className:
+          "bottom-0 right-0 flex fixed md:max-h-[300px] md:max-w-[420px] md:bottom-4 md:right-4",
+        variant: "destructive",
+        title: "Uh oh! You did not enter a receiving address.",
+        description: "Please enter a receiving address to continue.",
+      });
+      return;
+    }
+    if (receivingAddress) {
+      const isValidAddress = isAddress(receivingAddress, { strict: false });
+      if (isValidAddress) {
+        setIsValidAddress(true);
+      } else {
+        setIsValidAddress(false);
+      }
+    }
+    if (sendingAmount === "") {
+      toast({
+        className:
+          "bottom-0 right-0 flex fixed md:max-h-[300px] md:max-w-[420px] md:bottom-4 md:right-4",
+        variant: "destructive",
+        title: "Uh oh! You did not enter an amount to send.",
+        description: "Please enter an amount to send to continue.",
+      });
+      return;
+    }
+    if (sendingAmount) {
+      const isValidAmount =
+        parseEther(currentBalance) >= parseEther(sendingAmount);
+      if (isValidAmount) {
+        setIsValidAmount(true);
+        setContinueButtonLoading(true);
+        setTransactionCost("0");
+        const isValidTotal =
+          parseEther(currentBalance) >= parseEther(sendingAmount);
+        if (isValidTotal) {
+          setIsValidTotal(true);
+          setReadyToTransfer(true);
+        } else {
+          setIsValidTotal(false);
+          setReadyToTransfer(false);
+          return;
+        }
+        setContinueButtonLoading(false);
+      } else {
+        setIsValidAmount(false);
+        return;
+      }
+    }
+  }
+
   async function submitDelegatedTransaction() {
     /**
      * Retrieve the handle to the private key from some unauthenticated storage
@@ -374,6 +397,18 @@ export default function PayForm() {
       </div>
       <div className="flex flex-col gap-8 mt-4 mb-6">
         <div className="flex flex-col gap-2">
+          <Label htmlFor="receivingAddress">Network</Label>
+          <div className="flex flex-row gap-2 items-center justify-center">
+            <Input
+              id="receivingAddress"
+              className="rounded-none w-full border-primary border-2 p-2.5 mt-2"
+              placeholder="0x..."
+              value={selectChainNameFromNetwork(network)}
+              readOnly
+            />
+          </div>
+        </div>
+        <div className="flex flex-col gap-2">
           <Label htmlFor="receivingAddress">Receiving address</Label>
           <div className="flex flex-row gap-2 items-center justify-center">
             <Input
@@ -382,39 +417,7 @@ export default function PayForm() {
               placeholder="0x..."
               value={receivingAddress}
               readOnly
-              required
             />
-            <Dialog>
-              <DialogTrigger>
-                <ScanLine className="w-6 h-6" />
-              </DialogTrigger>
-              <DialogContent>
-                <DialogHeader>
-                  <DialogTitle>QR Scanner</DialogTitle>
-                  <DialogDescription>
-                    Scan QR code to autofill
-                  </DialogDescription>
-                </DialogHeader>
-                <Scanner
-                  onScan={(result) => handleQrScan(result[0].rawValue)}
-                />
-                <DialogFooter>
-                  <div className="flex flex-col items-center justify-center">
-                    {qrScanSuccess ? (
-                      <p className="flex flex-row gap-2 text-blue-600">
-                        <ThumbsUp className="h-6 w-6" />
-                        Scan completed
-                      </p>
-                    ) : (
-                      <p className="flex flex-row gap-2 text-yellow-600">
-                        <Loader2 className="h-6 w-6 animate-spin" />
-                        Scanning...
-                      </p>
-                    )}
-                  </div>
-                </DialogFooter>
-              </DialogContent>
-            </Dialog>
           </div>
         </div>
         <div className="flex flex-col gap-2">
@@ -426,7 +429,6 @@ export default function PayForm() {
             placeholder="0"
             value={sendingAmount}
             readOnly
-            required
           />
         </div>
         <div className="flex flex-col gap-2">
@@ -449,140 +451,130 @@ export default function PayForm() {
             <Label htmlFor="delegate-fee">Delegate gas fee</Label>
           </div>
         ) : null}
-        {!delegateFeeActive && (
-          <div className="flex flex-col gap-2 border-2 border-primary p-2">
-            <h2 className="border-b pb-1 text-md font-semibold">Details</h2>
-            <div className="flex flex-row gap-2">
-              <h3 className="text-sm text-muted-foreground">
-                Receiving address
-              </h3>
-              <p className="flex flex-row gap-2 items-center text-sm">
-                {receivingAddress
-                  ? truncateAddress(receivingAddress as Address, 4)
-                  : "-----"}
-                {isValidAddress === undefined ? null : isValidAddress ===
-                  true ? (
-                  <Popover>
-                    <PopoverTrigger>
-                      <Check className="w-4 h-4 text-green-500" />
-                    </PopoverTrigger>
-                    <PopoverContent className="w-fit text-green-500">
-                      Valid address
-                    </PopoverContent>
-                  </Popover>
-                ) : (
-                  <Popover>
-                    <PopoverTrigger>
-                      <CircleX className="w-4 h-4 text-red-500" />
-                    </PopoverTrigger>
-                    <PopoverContent className="w-fit text-red-500">
-                      Invalid address
-                    </PopoverContent>
-                  </Popover>
-                )}
-              </p>
-            </div>
-            <div className="flex flex-row gap-2">
-              <h3 className="text-sm text-muted-foreground">Sending amount</h3>
-              <p className="flex flex-row gap-2 items-center text-sm">
-                {`${
-                  sendingAmount ? formatBalance(sendingAmount, 18) : "-----"
-                } ${selectNativeAssetSymbol(network)}`}
-                {isValidAmount === undefined ? null : isValidAmount === true ? (
-                  <Popover>
-                    <PopoverTrigger>
-                      <Check className="w-4 h-4 text-green-500" />
-                    </PopoverTrigger>
-                    <PopoverContent className="w-fit text-green-500">
-                      Valid amount
-                    </PopoverContent>
-                  </Popover>
-                ) : (
-                  <Popover>
-                    <PopoverTrigger>
-                      <CircleX className="w-4 h-4 text-red-500" />
-                    </PopoverTrigger>
-                    <PopoverContent className="w-fit text-red-500">
-                      Invalid amount
-                    </PopoverContent>
-                  </Popover>
-                )}
-              </p>
-            </div>
-            <div className="flex flex-row gap-2">
-              <h3 className="text-sm text-muted-foreground">Estimated fees</h3>
-              <p className="flex flex-row gap-2 items-center text-sm">
-                <Fuel className="w-4 h-4" />
-                {`${
-                  transactionCost ? transactionCost : "-----"
-                } ${selectNativeAssetSymbol(network)}`}
-                {isValidTotal === undefined ? null : isValidTotal === true ? (
-                  <Popover>
-                    <PopoverTrigger>
-                      <Check className="w-4 h-4 text-green-500" />
-                    </PopoverTrigger>
-                    <PopoverContent className="w-fit text-green-500">
-                      Valid total
-                    </PopoverContent>
-                  </Popover>
-                ) : (
-                  <Popover>
-                    <PopoverTrigger>
-                      <CircleX className="w-4 h-4 text-red-500" />
-                    </PopoverTrigger>
-                    <PopoverContent className="w-fit text-red-500">
-                      Invalid total
-                    </PopoverContent>
-                  </Popover>
-                )}
-              </p>
-            </div>
-            {continueButtonLoading ? (
-              <Button disabled className="w-fit self-end">
-                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-                Please wait
-              </Button>
-            ) : (
-              <Button
-                disabled={readyToTransfer}
-                className="w-fit self-end"
-                onClick={prepareTransaction}
-              >
-                Continue
-              </Button>
-            )}
+        <div className="flex flex-col gap-2 border-2 border-primary p-2">
+          <h2 className="border-b pb-1 text-md font-semibold">Details</h2>
+          <div className="flex flex-row gap-2">
+            <h3 className="text-sm text-muted-foreground">Receiving address</h3>
+            <p className="flex flex-row gap-2 items-center text-sm">
+              {receivingAddress
+                ? truncateAddress(receivingAddress as Address, 4)
+                : "-----"}
+              {isValidAddress === undefined ? null : isValidAddress === true ? (
+                <Popover>
+                  <PopoverTrigger>
+                    <Check className="w-4 h-4 text-green-500" />
+                  </PopoverTrigger>
+                  <PopoverContent className="w-fit text-green-500">
+                    Valid address
+                  </PopoverContent>
+                </Popover>
+              ) : (
+                <Popover>
+                  <PopoverTrigger>
+                    <CircleX className="w-4 h-4 text-red-500" />
+                  </PopoverTrigger>
+                  <PopoverContent className="w-fit text-red-500">
+                    Invalid address
+                  </PopoverContent>
+                </Popover>
+              )}
+            </p>
           </div>
-        )}
+          <div className="flex flex-row gap-2">
+            <h3 className="text-sm text-muted-foreground">Sending amount</h3>
+            <p className="flex flex-row gap-2 items-center text-sm">
+              {`${
+                sendingAmount ? formatBalance(sendingAmount, 18) : "-----"
+              } ${selectNativeAssetSymbol(network)}`}
+              {isValidAmount === undefined ? null : isValidAmount === true ? (
+                <Popover>
+                  <PopoverTrigger>
+                    <Check className="w-4 h-4 text-green-500" />
+                  </PopoverTrigger>
+                  <PopoverContent className="w-fit text-green-500">
+                    Valid amount
+                  </PopoverContent>
+                </Popover>
+              ) : (
+                <Popover>
+                  <PopoverTrigger>
+                    <CircleX className="w-4 h-4 text-red-500" />
+                  </PopoverTrigger>
+                  <PopoverContent className="w-fit text-red-500">
+                    Invalid amount
+                  </PopoverContent>
+                </Popover>
+              )}
+            </p>
+          </div>
+          <div className="flex flex-row gap-2">
+            <h3 className="text-sm text-muted-foreground">Estimated fees</h3>
+            <p className="flex flex-row gap-2 items-center text-sm">
+              <Fuel className="w-4 h-4" />
+              {`${
+                transactionCost ? transactionCost : "-----"
+              } ${selectNativeAssetSymbol(network)}`}
+              {isValidTotal === undefined ? null : isValidTotal === true ? (
+                <Popover>
+                  <PopoverTrigger>
+                    <Check className="w-4 h-4 text-green-500" />
+                  </PopoverTrigger>
+                  <PopoverContent className="w-fit text-green-500">
+                    Valid total
+                  </PopoverContent>
+                </Popover>
+              ) : (
+                <Popover>
+                  <PopoverTrigger>
+                    <CircleX className="w-4 h-4 text-red-500" />
+                  </PopoverTrigger>
+                  <PopoverContent className="w-fit text-red-500">
+                    Invalid total
+                  </PopoverContent>
+                </Popover>
+              )}
+            </p>
+          </div>
+          {continueButtonLoading ? (
+            <Button disabled className="w-fit self-end">
+              <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              Please wait
+            </Button>
+          ) : delegateFeeActive ? (
+            <Button
+              disabled={readyToTransfer}
+              className="w-fit self-end"
+              onClick={prepareDelegatedTransaction}
+            >
+              Continue
+            </Button>
+          ) : (
+            <Button
+              disabled={readyToTransfer}
+              className="w-fit self-end"
+              onClick={prepareTransaction}
+            >
+              Continue
+            </Button>
+          )}
+        </div>
       </div>
       {sendButtonLoading ? (
-        <div className="flex flex-row gap-2 justify-between">     
-          <Button disabled variant="outline">
-            <Ban className="mr-2 h-4 w-4" />
-            Cancel
-          </Button>
-          <Button className="w-[150px]" disabled>
-            <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-            Please wait
-          </Button>
-        </div>
+        <Button className="w-[150px] self-end" disabled>
+          <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+          Please wait
+        </Button>
       ) : (
-        <div className="flex flex-row gap-2 justify-between">
-          <Button variant="outline">
-            <Ban className="mr-2 h-4 w-4" />
-            Cancel
-          </Button>
-          <Button
-            disabled={!readyToTransfer}
-            onClick={
-              delegateFeeActive ? submitDelegatedTransaction : submitTransaction
-            }
-            className="w-[150px]"
-          >
-            <Send className="mr-2 h-4 w-4" />
-            Pay
-          </Button>
-        </div>
-
+        <Button
+          disabled={!readyToTransfer}
+          onClick={
+            delegateFeeActive ? submitDelegatedTransaction : submitTransaction
+          }
+          className="w-[150px] self-end"
+        >
+          <HandCoins className="mr-2 h-4 w-4" />
+          Pay
+        </Button>
       )}
     </div>
   );
