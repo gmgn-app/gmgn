9b82d4cbb6eb9b062d4518ede858c0da0664a1cf
ZxStim <thang.victor.luong@gmail.com>
2024-11-05 17:10:50 +0700

    updated receive page

--- app/request/page.tsx
+++ app/request/page.tsx
@@ -1,20 +1,13 @@
 "use client";
 
 import Link from "next/link";
-import Image from "next/image";
-import { useSearchParams } from "next/navigation";
 import BackButton from "@/components/back-button";
 import RequestForm from "@/components/request-form";
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Header from "@/components/header";
-import { constructNavUrl } from "@/lib/utils";
 
 
 export default function RequestPage() {
-  const searchParams = useSearchParams();
-  const address = searchParams.get("address");
-  const network = searchParams.get("network");
-  const token = searchParams.get("token");
 
   return (
     <div className="flex flex-col gap-6 p-4 w-screen md:w-[768px]">
@@ -22,11 +15,11 @@ export default function RequestPage() {
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
         Request
       </h1>
-      <BackButton route={constructNavUrl("/", network, address)} />
+      <BackButton route="/" />
       <Tabs defaultValue="request" className="w-full">
         <TabsList className="grid w-full grid-cols-2">
           <TabsTrigger asChild value="receive">
-            <Link href={constructNavUrl("/receive", network, address, token)}>
+            <Link href="/receive">
               Receive
             </Link>
           </TabsTrigger>
