a3eb21e77bab78a0c1a4879089d39d373e7a4bd8
ZxStim <thang.victor.luong@gmail.com>
2024-11-05 17:10:08 +0700

    updated send page

--- app/send/page.tsx
+++ app/send/page.tsx
@@ -1,19 +1,14 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
+
 import SendTransactionForm from "@/components/send-transaction-form";
 import Link from "next/link";
 import BackButton from "@/components/back-button";
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Header from "@/components/header";
-import { constructNavUrl } from "@/lib/utils";
 
 
 export default function SendPage() {
-  const searchParams = useSearchParams();
-  const address = searchParams.get("address");
-  const network = searchParams.get("network");
-  const token = searchParams.get("token");
 
   return (
     <div className="flex flex-col gap-6 p-4 w-screen md:w-[768px]">
@@ -21,12 +16,12 @@ export default function SendPage() {
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
         Send
       </h1>
-      <BackButton route={constructNavUrl("/", network, address)} />
+      <BackButton route="/" />
       <Tabs defaultValue="send" className="w-full">
         <TabsList className="grid w-full grid-cols-2">
           <TabsTrigger value="send">Send</TabsTrigger>
           <TabsTrigger asChild value="message">
-            <Link href={constructNavUrl("/message", network, address, token)}>
+            <Link href="/message">
               Message
             </Link>
           </TabsTrigger>
