0ca16d688a27375b12993508f11ce54f9aa2abd0
ZxStim <thang.victor.luong@gmail.com>
2024-11-06 15:00:26 +0700

    fixed trade page

--- app/trade/page.tsx
+++ app/trade/page.tsx
@@ -1,15 +1,8 @@
-"use client";
-
-import { useSearchParams } from "next/navigation";
 import BackButton from "@/components/back-button";
 import NavBar from "@/components/navbar";
-import { constructNavUrl } from "@/lib/utils";
 import Header from "@/components/header";
 
 export default function TradePage() {
-  const searchParams = useSearchParams();
-  const address = searchParams.get("address");
-  const network = searchParams.get("network");
 
   return (
     <div className="flex flex-col gap-6 p-4 w-screen md:w-[768px]">
@@ -17,7 +10,7 @@ export default function TradePage() {
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
         Trade
       </h1>
-      <BackButton route={constructNavUrl("/", network, address)} />
+      <BackButton route="/" />
       <NavBar />
     </div>
   );
