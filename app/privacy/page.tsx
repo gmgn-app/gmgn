6ad86ded8e6afdf0f1c861da6a944f1b0d1ddb1b
ZxStim <thang.victor.luong@gmail.com>
2024-11-06 14:57:46 +0700

    fixed privacy page

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -1,14 +1,8 @@
-"use client";
-
-import { useSearchParams } from "next/navigation";
 import BackButton from "@/components/back-button";
 import { constructNavUrl } from "@/lib/utils";
 import Header from "@/components/header";
 
 export default function ProfilePage() {
-  const searchParams = useSearchParams();
-  const address = searchParams.get("address");
-  const network = searchParams.get("network");
 
   return (
     <div className="flex flex-col gap-6 p-4 w-screen md:w-[768px]">
@@ -16,7 +10,7 @@ export default function ProfilePage() {
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
         Privacy
       </h1>
-      <BackButton route={constructNavUrl("/", network, address)} />
+      <BackButton route="/" />
       <p>
         This Privacy Policy describes Our policies and procedures on the
         collection, use and disclosure of Your information when You use the
