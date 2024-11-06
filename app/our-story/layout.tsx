c3a35669b4705aa28a96d3a4a51136c35ef8b999
ZxStim <thang.victor.luong@gmail.com>
2024-11-06 14:58:48 +0700

    fixed our story page

--- app/our-story/layout.tsx
+++ app/our-story/layout.tsx
@@ -1,14 +1,8 @@
-"use client";
-
-import { useSearchParams } from "next/navigation";
 import BackButton from "@/components/back-button";
 import Header from "@/components/header";
-import { constructNavUrl } from "@/lib/utils";
 
 export default function MdxLayout({ children }: { children: React.ReactNode }) {
-  const searchParams = useSearchParams();
-  const address = searchParams.get("address");
-  const network = searchParams.get("network");
+
   // Create any shared layout or styles here
   return (
     <div className="flex flex-col gap-6 p-4 w-screen md:w-[768px]">
@@ -16,7 +10,7 @@ export default function MdxLayout({ children }: { children: React.ReactNode }) {
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
         Our story
       </h1>
-      <BackButton route={constructNavUrl("/", network, address)} />
+      <BackButton route="/" />
       {children}
     </div>
   );
