a3fe92b1fd30b4e5e622de96278ae284763935a6
ZxStim <thang.victor.luong@gmail.com>
2024-11-08 15:46:36 +0700

    working fix landing page

--- app/loading.tsx
+++ app/loading.tsx
@@ -6,10 +6,7 @@ export default function Loading() {
     <div className="flex flex-col gap-6 p-4 w-screen md:w-[768px]">
       <div className="flex flex-row gap-2 items-center justify-between">
         <Skeleton className="w-[40px] h-[40px] rounded-md" />
-        <div className="flex flex-row gap-2">
-          <Skeleton className="w-[250px] h-[40px] rounded-md" />
-          <Skeleton className="w-[40px] h-[40px] rounded-md" />
-        </div>
+        <Skeleton className="w-[40px] h-[40px] rounded-md" />
       </div>
       <Skeleton className="w-full h-[210px] rounded-md" />
       <div className="grid grid-cols-2 lg:grid-cols-4 gap-2">
